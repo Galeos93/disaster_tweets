d7563d559341c751d067cb986724cd101fb5b5a4
agarcia <alejandro.garcia.ihs@gmail.com>
2021-06-22 22:26:13 +0200

    Draft of TweetDataLoader

--- disaster_tweets/data_loader/data_loaders.py
+++ disaster_tweets/data_loader/data_loaders.py
@@ -1,4 +1,9 @@
+from numpy import dtype
+import pandas as pd
+import torch
+from torch.utils.data.dataloader import DataLoader
 from torchvision import datasets, transforms
+
 from base import BaseDataLoader
 
 
@@ -14,3 +19,34 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+class DataPreprocessor:
+    def __init__(self, tokenizer, vocab, postprocessor):
+        self.tokenizer = tokenizer
+        self.vocab = vocab
+        self.postprocessor = postprocessor
+
+    def __call__(self, sentences_iter):
+        tokenized_sentences = [self.tokenizer(x) for x in sentences_iter]
+        vectorized_sentences = [torch.tensor(self.vocab(x), dtype=torch.long) 
+                                for x in tokenized_sentences]
+        return torch.cat(vectorized_sentences)
+
+
+
+class TweetDataLoader(DataLoader):
+    """Loads Tweets from a DataFrame.
+    
+    Notes
+    -----
+    The objective of class is:
+
+    (1) Convert the sentences into a series of tokens
+    (2) Convert these tokens into integers, which are part of a Vocabulary
+    (3) Postprocess the sentences so they have the same length?
+    (4) Batchify them    
+    
+    """
+    def __init__(self, csv_path, data_preprocessor):
+        df = pd.read_csv(csv_path)
+        data = data_preprocessor(df.text.tolist())
---
+++ tests/data_loaders/test_data_loaders.py
@@ -0,0 +1,8 @@
+from disaster_tweets.data_loader import data_loaders
+
+
+class TestTweetDataLoader:
+    pass
+
+class TestDataPreprocessor:
+    pass
